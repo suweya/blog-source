88e6b1cd4b07a333868529b59460f7f3dc2d4ff1
suweya <suweya89@gmail.com>
2017-02-08 23:44:31 +0800

    update: logo icon

--- react/src/routes/login/login.js
+++ react/src/routes/login/login.js
@@ -17,40 +17,35 @@ const Login = ({ loading, dispatch, form: { getFieldDecorator, validateFields } 
     <LoginLayout>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <Icon className={styles['logo-img']} type="code" />
+          <Icon className={styles['logo-img']} type="code-o" />
           <span>blog</span>
         </div>
         <Form onSubmit={onSubmit}>
           <Form.Item>
             {
-                            getFieldDecorator('username', {
-                              rules: [{ required: true, message: '请输入用户名' }],
-                            })(<Input addonBefore={<Icon type="user" />} placeholder="用户名" />)
-                        }
+              getFieldDecorator('username', {
+                rules: [{ required: true, message: '请输入用户名' }],
+              })(<Input addonBefore={<Icon type="user" />} placeholder="用户名" />)
+            }
           </Form.Item>
           <Form.Item>
             {
-                            getFieldDecorator('password', {
-                              rules: [{ required: true, message: '请输入密码' }],
-                            })(<Input addonBefore={<Icon type="lock" />} type="password" placeholder="密码" />)
-                        }
+              getFieldDecorator('password', {
+                rules: [{ required: true, message: '请输入密码' }],
+              })(<Input addonBefore={<Icon type="lock" />} type="password" placeholder="密码" />)
+            }
           </Form.Item>
           <Form.Item>
             {
-                            getFieldDecorator('remember', {
-                              valuePropName: 'checked',
-                              initialValue: true,
-                            })(<Checkbox>记住我</Checkbox>)
-                        }
+              getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>记住我</Checkbox>)
+            }
             <span className={styles['login-form-forgot']}><Link to="/forgot">忘记密码</Link></span>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className={styles['login-form-button']}
-              loading={loading}
-            >
-                            登录
-                        </Button>
+            <Button type="primary" htmlType="submit" className={styles['login-form-button']} loading={loading}>
+              登录
+            </Button>
             <span>或 <Link to="/register">注册</Link></span>
           </Form.Item>
         </Form>
